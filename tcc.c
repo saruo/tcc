055a641688a528deb0ed07cda80a16d4a5ad34bb
Monkey Planet <sample@example.com>
2020-02-18 19:11:42 +0900

    足算引き算を追加。

--- tcc.c
+++ tcc.c
@@ -9,11 +9,38 @@ int main( int argc, char **argv )
         return 1;
     }
 
+    char *p = argv[1];
+
     // アセンブラの出力
     printf(".intel_syntax noprefix\n");
     printf(".global main\n");
     printf("main:\n");
-    printf("  mov rax, %d\n", atoi(argv[1]));
+
+    // 最初に数字がきている前提
+    printf("  mov rax, %ld\n", strtol(p, &p, 10));
+
+    while( *p )
+    {
+        if( *p == '+' )
+        {
+            // 続く数値を取り出して足し合わせるコード
+            ++p;
+            printf("  add rax, %ld\n", strtol(p, &p, 10));
+            continue;
+        }
+
+        if( *p == '-' )
+        {
+            // 続く数値を取り出して引き算するコード
+            ++p;
+            printf("  sub rax, %ld\n", strtol(p, &p, 10));
+            continue;
+        }
+
+        fprintf(stderr, "unexpected character: '%c'\n", *p);
+        return 1;
+    }
+
     printf("  ret\n");
 
     return 0;
